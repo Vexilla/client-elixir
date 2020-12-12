33e6ed60edd320b1838478d8660ced8085a4e8fb
Chris Griffing <cmgriffing@gmail.com>
2020-12-12 10:04:33 -0800

    fix: roll back httpoison version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule VexillaClientElixir.MixProject do
   def project do
     [
       app: :vexilla_client_elixir,
-      version: "0.0.1",
+      version: "0.0.2",
       elixir: "~> 1.5",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
@@ -32,7 +32,7 @@ defmodule VexillaClientElixir.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
-      {:httpoison, "~> 1.7"},
+      {:httpoison, "~> 1.6"},
       {:jason, "~> 1.2"},
       {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
     ]
